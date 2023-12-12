85b839e06274248caf5d66c33c9aa3264518962c
sadat92 <sadatiokeyo45@gmail.com>
2023-12-12 00:39:05 -0800

    first commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    op = argv[2]
-    ops = {
-            "+": add(a, b),
-            "-": sub(a, b),
-            "*": mul(a, b),
-            "/": div(a, b)}
-    if op in ops.keys():
-        print("{:d} {} {:d} = {:d}".format(a, op, b, ops[op]))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+def weight_average(my_list=[]):
+    if not my_list or my_list is None:
+        return 0
+    totalV = 0
+    weight = 0
+    for i in my_list:
+        totalV += i[0] * i[1]
+        weight += i[1]
+    return totalV / weight
